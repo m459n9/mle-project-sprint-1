5daa02d647a56af51137f4f309e3117ef86aa057
Maksim Staroverov <mstaroverov6@gmail.com>
2026-09-22 23:15:57 +0300

    Починил отправку в Telegram: беру dag_id, а не объект DAG, иначе угловые скобки ломают сообщение

--- part1_airflow/plugins/steps/messages.py
+++ part1_airflow/plugins/steps/messages.py
@@ -7,16 +7,16 @@ from airflow.providers.telegram.hooks.telegram import TelegramHook
 
 def send_telegram_success_message(context):
     hook = TelegramHook(token=os.environ['TELEGRAM_TOKEN'], chat_id=os.environ['TELEGRAM_CHAT_ID'])
-    dag = context['dag']
+    dag_id = context['dag'].dag_id
     run_id = context['run_id']
-    message = f'Исполнение DAG {dag} с id={run_id} прошло успешно!'
+    message = f'Исполнение DAG {dag_id} с id={run_id} прошло успешно!'
     hook.send_message({'chat_id': os.environ['TELEGRAM_CHAT_ID'], 'text': message})
 
 
 def send_telegram_failure_message(context):
     hook = TelegramHook(token=os.environ['TELEGRAM_TOKEN'], chat_id=os.environ['TELEGRAM_CHAT_ID'])
-    dag = context['dag']
+    dag_id = context['dag'].dag_id
     run_id = context['run_id']
     task_instance_key_str = context['task_instance_key_str']
-    message = f'Исполнение DAG {dag} с id={run_id} прошло с ошибкой в задаче {task_instance_key_str}!'
+    message = f'Исполнение DAG {dag_id} с id={run_id} прошло с ошибкой в задаче {task_instance_key_str}!'
     hook.send_message({'chat_id': os.environ['TELEGRAM_CHAT_ID'], 'text': message})
